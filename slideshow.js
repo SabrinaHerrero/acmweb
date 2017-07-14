34b2b24f0d137ecebbaa010ca9fea563b576c3da
SabrinaHerrero <sabrinaherrero123@gmail.com>
2017-07-13 23:02:00 -0500

    initial commit: website layout draft

---
+++ slideshow.js
@@ -0,0 +1,47 @@
+var slideIndex = 0;
+var nanoseconds = 4000;
+timedSlideShow();
+var timer = setInterval(function() {timedSlideShow();}, nanoseconds);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  clearInterval(timer);
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";  
+  timer = setInterval(function() {timedSlideShow();}, nanoseconds); 
+}
+
+function timedSlideShow() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
