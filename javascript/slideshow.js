db8f7b775d0c7e936b4c9fbcc503d23c9e960bfe
Chia Hua Lu <thegoldflute@users.noreply.github.com>
2017-09-17 12:07:57 -0500

    Code Cleanup
    Footer Extracted to external file (to ensure consistency).
    Slideshow script extracted to external file.

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -3,6 +3,7 @@ var nanoseconds = 4000;
 timedSlideShow();
 var timer = setInterval(function() {timedSlideShow();}, nanoseconds);
 
+//setTimeout(timedSlideShow(), 2000);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -14,11 +15,13 @@ function currentSlide(n) {
 
 function showSlides(n) {
   clearInterval(timer);
+  //document.getElementById("slideIndexI").innerHTML = slideIndex;
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+  //document.getElementById("slideIndexF").innerHTML = slideIndex;
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
@@ -44,4 +47,5 @@ function timedSlideShow() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
+    //setTimeout(timedSlideShow, 2000); // Change image every 2 seconds
 }
